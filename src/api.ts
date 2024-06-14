481e7bde01ec024bc3617a902b771da692d1c336
Nea <38318241+nealol@users.noreply.github.com>
2024-06-14 01:01:41 -0400

    Proxy again

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import { URLSearchParams } from 'url'
 import { setTimeout as setTimeoutAsync } from 'timers/promises'
 import EventEmitter from 'events'
 
-let proxyEndpoint = 'https://api.mojang.com/users/profiles/minecraft'
+let proxyEndpoint = 'https://api.lilith.rip/mojang/profiles'
 export function setProxyEndpoint(endpoint: string) {
     proxyEndpoint = endpoint
 }
